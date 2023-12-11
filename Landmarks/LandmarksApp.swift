3fbe09684cbc99e8de3c8df9940cbc1d129e2cfa
Ram Jondhale <ramjondhale1@gmail.com>
2023-12-11 12:39:11 +0530

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Ram Jondhale on 11/12/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
